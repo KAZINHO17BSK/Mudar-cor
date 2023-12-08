b717a122d8e3c11040a4e0681c72b04feb4f10f8
Charles Augusto <144728325+KAZINHO17BSK@users.noreply.github.com>
2023-12-07 21:01:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var count = 0;
+
+function toggleVisibility() {
+  var hiddenDiv = document.getElementById("hiddenDiv");
+  if (hiddenDiv.style.display === "none") {
+    hiddenDiv.style.display = "block";
+  } else {
+    hiddenDiv.style.display = "none";
+  }
+}
+
+function changeColor() {
+  document.body.style.backgroundColor = getRandomColor();
+}
+
+function increaseCounter() {
+  count++;
+  document.getElementById("count").innerText = count;
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
